2945811710bf69daedc1ae32c8a77194b33340c7
VasiliyBogdanov <moproq@icloud.com>
2022-02-27 12:04:25 +0300

    Update

--- tests/fixtures/results.py
+++ tests/fixtures/results.py
@@ -65,51 +65,6 @@ stylish_flat_result = """{
   + verbose: true
 }"""
 
-stylish_nested_result = """{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}"""
-
 plain_flat_result = """Property 'follow' was removed
 Property 'proxy' was removed
 Property 'timeout' was updated. From 50 to 20
